9b6d2f0a4ea60aa80c9b109fa49d09d7fe5cf79b
RajviKamani <rajvikamani2211@gmail.com>
2025-01-28 18:38:49 +0530

    individual created and storybook added

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,7 +16,6 @@ export default {
         "bg-gray-13":"#6B7280",
         "bg-blue-14":"#CECBFF" ,
         "bg-gray-11":"#F6F5FF",
-        "bg-blue-14":"#CECBFF",
         "bg-red":"#FFE5E4",
         "text-red":"#E53935",
         "follow_ups_yellow":"#FFC107",
