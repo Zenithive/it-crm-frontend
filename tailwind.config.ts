8b847ded07286d8c91e4532364ca236a60aa0cbf
srt-1909 <sachinthakkar1992002@gmail.com>
2025-01-29 11:06:54 +0530

    tailwind.config conflig solve

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,12 +14,13 @@ export default {
         "bg-blue-11":"#8780FF",
         "bg-blue-12":"#6158FF",
         "bg-gray-13":"#6B7280",
-        "bg-blue-14":"#CECBFF",
+        // storybook
+        "bg-blue-14":"#CECBFF" ,
+        "bg-gray-11":"#F6F5FF",
         "bg-red":"#FFE5E4",
         "text-red":"#E53935",
         "follow_ups_yellow":"#FFC107",
         "follow_ups_green":"#28A745"
-
       },
       scale: {
         '102.5': '1.008', 
