4d48deef13ddfd02eaa48e6b89dc52fa6d8364ef
Arati Dangar <aratid@zenithive.com>
2025-01-29 12:48:17 +0530

    change

--- app/components/Dashboard.tsx
+++ app/components/Dashboard.tsx
@@ -12,23 +12,7 @@ import { title } from "process";
 
 
 export default function DashboardPage() {
-
-  const [checkedState, setCheckedState] = useState<{
-    task1: boolean,
-    task2: boolean,
-    task3: boolean,
-  }>({
-    task1: false,
-    task2: false,
-    task3: false,
-  });
-
-  const handleCheckboxChange = (taskName: any) => {
-    setCheckedState((prevState) => ({
-      ...prevState,
-      [taskName]: !prevState[taskName],
-    }));
-
+  const [isChecked, setIsChecked] = useState(false);
 
   const title = [
     {
@@ -36,184 +20,203 @@ export default function DashboardPage() {
     }
   ]
 
- 
+  const handleCheckboxChange = (event:any) => {
+    setIsChecked(event.target.checked);
   };
 
   return (
     <>
       <Navbar></Navbar>
 
-
-     
-   <div className="">
+      <div className="">
         <Title title={title[0].titleName} button={title[0].button}></Title>
       </div>
-      <div className="flex  gap-[20px]">
-        <LeftCardDetails />
-        <div className="cards flex gap-[20px] ">
-          <Card
-            logo={"tabler_checkbox.svg"}
-            task={"Today’s Task"}
-            lastText={"5 remaining"}
-          >
-            <div className="flex gap-[18px] flex-col">
-              <div className="flex gap-[22px] pt-[20px]  px-[10px] ">
-                <div className="">
-                  <input
-                    type="checkbox"
-                    checked={checkedState.task1}
-                    onChange={() => handleCheckboxChange("task1")}
-                    className="w-[19px] h-[17px] mt-2 text-blue-600 border-gray-300  bg-blue-111 focus:ring-blue-500 "
-                  />
-                </div>
-                <div className="">
-                  <div className="flex flex-col text-[16px]">
-                    <span style={{ color: "#333333" }}>
-                      Follow up with Client ABC
-                    </span>
-                    <span style={{ color: "#6B7280" }}>Due in 2 hours</span>
+
+      <div className="flex ">
+        <div className="">
+          <LeftCardDetails />
+        </div>
+
+        <div className="ml-[100px] mt-[35px]">
+         
+          <div className="flex gap-[20px]">
+            {/* <LeftCardDetails/> */}
+            <div className="cards flex gap-[20px] ">
+              <Card
+                logo={"tabler_checkbox.svg"}
+                task={"Today’s Task"}
+                lastText={"5 remaining"}
+              >
+                <div className="flex gap-[18px] flex-col">
+                  <div className="flex gap-[22px] pt-[20px]  px-[10px] ">
+                    <div className="">
+                      <input
+                        type="checkbox"
+                        checked={isChecked}
+                        onChange={handleCheckboxChange}
+                        className="w-[19px] h-[17px] mt-2 text-blue-600 border-gray-300   focus:ring-blue-500 "
+                      />
+                    </div>
+                    <div className="">
+                      <div className="flex flex-col text-[16px]">
+                        <span style={{ color: "#333333" }}>
+                          Follow up with Client ABC
+                        </span>
+                        <span style={{ color: "#6B7280" }}>Due in 2 hours</span>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="flex gap-4   px-[10px] ">
+                    <div className="">
+                      <input
+                        type="checkbox"
+                        checked={isChecked}
+                        onChange={handleCheckboxChange}
+                        className="w-[19px] h-[17px] mt-2 text-blue-600 border-gray-300  focus:ring-blue-500 "
+                      />
+                    </div>
+                    <div className="">
+                      <div className="flex flex-col text-[16px]">
+                        <span style={{ color: "#333333" }}>
+                          Follow up with Client ABC
+                        </span>
+                        <span style={{ color: "#6B7280" }}>Due in 2 hours</span>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="flex gap-4  px-[10px] ">
+                    <div className="">
+                      <input
+                        type="checkbox"
+                        checked={isChecked}
+                        onChange={handleCheckboxChange}
+                        className="w-[19px] h-[17px] mt-2 text-blue-600 border-gray-300  focus:ring-blue-500 "
+                      />
+                    </div>
+                    <div className="">
+                      <div className="flex flex-col text-[16px]">
+                        <span style={{ color: "#333333" }}>
+                          Follow up with Client ABC
+                        </span>
+                        <span style={{ color: "#6B7280" }}>Due in 2 hours</span>
+                      </div>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div className="flex gap-4   px-[10px] ">
-                <div className="">
-                  <input
-                    type="checkbox"
-                    checked={checkedState.task2}
-                    onChange={() => handleCheckboxChange("task2")}
-                    className="w-[19px] h-[17px] mt-2 text-blue-600 border-gray-300  focus:ring-blue-500 "
-                  />
-                </div>
-                <div className="">
-                  <div className="flex flex-col text-[16px]">
-                    <span style={{ color: "#333333" }}>
-                      Follow up with Client ABC
-                    </span>
-                    <span style={{ color: "#6B7280" }}>Due in 2 hours</span>
+              </Card>
+              <Card
+                logo={"video.svg"}
+                task={"Today’s Meetings"}
+                lastText={"View All"}
+                lastTextColor="text-bg-blue-11"
+              >
+                <div className="flex gap-[18px] flex-col">
+                  <div className="flex gap-4 pt-[20px] px-[10px] ">
+                    <img src="Frame 423.svg"></img>
+
+                    <div className="">
+                      <div className="flex flex-col ">
+                        <span
+                          style={{ color: "#333333" }}
+                          className="text-[14px] font-normal"
+                        >
+                          Product Demo - XYZ Corp
+                        </span>
+                        <span
+                          style={{ color: "#6B7280" }}
+                          className="text-[10px]"
+                        >
+                          2:00 PM - 3:00 PM
+                        </span>
+                      </div>
+                    </div>
+                    <div className="flex justify-between items-center">
+                      <button className=" w-[38px] h-[21px] bg-bg-blue-11 text-[10px] text-center rounded-md text-white">
+                        join
+                      </button>
+                    </div>
+                  </div>
+
+                  <div className="flex gap-4 px-[10px] ">
+                    <img src="Frame 423.svg"></img>
+
+                    <div className="">
+                      <div className="flex flex-col ">
+                        <span
+                          style={{ color: "#333333" }}
+                          className="text-[14px] font-normal"
+                        >
+                          Product Demo - XYZ Corp
+                        </span>
+                        <span
+                          style={{ color: "#6B7280" }}
+                          className="text-[10px]"
+                        >
+                          2:00 PM - 3:00 PM
+                        </span>
+                      </div>
+                    </div>
+                    <div className="flex justify-between items-center">
+                      <button className=" w-[38px] h-[21px] bg-bg-blue-11 text-[10px] text-center rounded-md text-white">
+                        join
+                      </button>
+                    </div>
+                  </div>
+
+                  <div className="flex gap-4  px-[10px] ">
+                    <img src="Frame 423.svg"></img>
+
+                    <div className="">
+                      <div className="flex flex-col ">
+                        <span
+                          style={{ color: "#333333" }}
+                          className="text-[14px] font-normal"
+                        >
+                          Product Demo - XYZ Corp
+                        </span>
+                        <span
+                          style={{ color: "#6B7280" }}
+                          className="text-[10px]"
+                        >
+                          2:00 PM - 3:00 PM
+                        </span>
+                      </div>
+                    </div>
+                    <div className="flex justify-between items-center">
+                      <button className=" w-[38px] h-[21px] bg-bg-blue-11 text-[10px] text-center rounded-md text-white">
+                        join
+                      </button>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div className="flex gap-4  px-[10px] ">
-                <div className="">
-                  <input
-                    type="checkbox"
-                    checked={checkedState.task3}
-                    onChange={() => handleCheckboxChange("task3")}
-                    className="w-[19px] h-[17px] mt-2 text-blue-600 border-gray-300  focus:ring-blue-500 "
-                  />
-                </div>
-                <div className="">
-                  <div className="flex flex-col text-[16px]">
-                    <span style={{ color: "#333333" }}>
-                      Follow up with Client ABC
-                    </span>
-                    <span style={{ color: "#6B7280" }}>Due in 2 hours</span>
+              </Card>
+
+              <Card logo={"cl.svg"} task={"Time"} lastText={""}>
+                <div className="flex gap-[25px] flex-col">
+                  <div className="flex justify-between pt-[20px]  px-[11px] ">
+                    <div className="text-[16px" style={{ color: "#6A6A6A" }}>
+                      New York
+                    </div>
+                    <div className="text-[12px] text-black">12:00 PM</div>
+                  </div>
+                  <div className="flex justify-between   px-[11px] ">
+                    <div className="text-[16px" style={{ color: "#6A6A6A" }}>
+                      London
+                    </div>
+                    <div className="text-[12px] text-black">12:00 PM</div>
+                  </div>
+                  <div className="flex justify-between   px-[11px] ">
+                    <div className="text-[16px" style={{ color: "#6A6A6A" }}>
+                      Tokyo
+                    </div>
+                    <div className="text-[12px] text-black">12:00 PM</div>
                   </div>
                 </div>
-              </div>
+              </Card>
             </div>
-          </Card>
-          <Card
-            logo={"video.svg"}
-            task={"Today’s Meetings"}
-            lastText={"View All"}
-            lastTextColor="text-bg-blue-11"
-          >
-            <div className="flex gap-[18px] flex-col">
-              <div className="flex gap-4 pt-[20px] px-[10px] ">
-                <img src="Frame 423.svg"></img>
+          </div>
 
-                <div className="">
-                  <div className="flex flex-col ">
-                    <span
-                      style={{ color: "#333333" }}
-                      className="text-[14px] font-normal"
-                    >
-                      Product Demo - XYZ Corp
-                    </span>
-                    <span style={{ color: "#6B7280" }} className="text-[10px]">
-                      2:00 PM - 3:00 PM
-                    </span>
-                  </div>
-                </div>
-                <div className="flex justify-between items-center">
-                  <button className=" w-[38px] h-[21px] bg-bg-blue-11 text-[10px] text-center rounded-md text-white">
-                    join
-                  </button>
-                </div>
-              </div>
-
-              <div className="flex gap-4 px-[10px] ">
-                <img src="Frame 423.svg"></img>
-
-                <div className="">
-                  <div className="flex flex-col ">
-                    <span
-                      style={{ color: "#333333" }}
-                      className="text-[14px] font-normal"
-                    >
-                      Product Demo - XYZ Corp
-                    </span>
-                    <span style={{ color: "#6B7280" }} className="text-[10px]">
-                      2:00 PM - 3:00 PM
-                    </span>
-                  </div>
-                </div>
-                <div className="flex justify-between items-center">
-                  <button className=" w-[38px] h-[21px] bg-bg-blue-11 text-[10px] text-center rounded-md text-white">
-                    join
-                  </button>
-                </div>
-              </div>
-
-              <div className="flex gap-4  px-[10px] ">
-                <img src="Frame 423.svg"></img>
-
-                <div className="">
-                  <div className="flex flex-col ">
-                    <span
-                      style={{ color: "#333333" }}
-                      className="text-[14px] font-normal"
-                    >
-                      Product Demo - XYZ Corp
-                    </span>
-                    <span style={{ color: "#6B7280" }} className="text-[10px]">
-                      2:00 PM - 3:00 PM
-                    </span>
-                  </div>
-                </div>
-                <div className="flex justify-between items-center">
-                  <button className=" w-[38px] h-[21px] bg-bg-blue-11 text-[10px] text-center rounded-md text-white">
-                    join
-                  </button>
-                </div>
-              </div>
-            </div>
-          </Card>
-
-          <Card logo={"cl.svg"} task={"Time"} lastText={""}>
-            <div className="flex gap-[25px] flex-col">
-              <div className="flex justify-between pt-[20px]  px-[11px] ">
-                <div className="text-[16px" style={{ color: "#6A6A6A" }}>
-                  New York
-                </div>
-                <div className="text-[12px] text-black">12:00 PM</div>
-              </div>
-              <div className="flex justify-between   px-[11px] ">
-                <div className="text-[16px" style={{ color: "#6A6A6A" }}>
-                  London
-                </div>
-                <div className="text-[12px] text-black">12:00 PM</div>
-              </div>
-              <div className="flex justify-between   px-[11px] ">
-                <div className="text-[16px" style={{ color: "#6A6A6A" }}>
-                  Tokyo
-                </div>
-                <div className="text-[12px] text-black">12:00 PM</div>
-              </div>
-            </div>
-          </Card>
-           <div className="flex gap-[20px]">
+        <div className="flex gap-[20px]">
             <div className="mt-[20px]">
               <RecentMeetings></RecentMeetings>
             </div>
@@ -226,9 +229,9 @@ export default function DashboardPage() {
           </div>
 
         </div>
-        </div>
 
-   
+
+      </div>
     </>
   );
 }
--- app/components/Login.tsx
+++ app/components/Login.tsx
@@ -1,5 +1,6 @@
-"use client";
 
+
+"use client";
 import Link from "next/link";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
@@ -15,14 +16,14 @@ export default function Login() {
       .max(50, "Email is too long")
       .email("Enter Valid Email ID")
       .matches(
-        /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+        /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, 
         "Please provide a valid email address (example@domain.com)"
       )
       .required("Email is required"),
     password: Yup.string()
       .min(6, "Should be at least 6 characters")
       .matches(
-        /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{6,})/,
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{6,})/, 
         "Must contain 6 characters, one uppercase, one lowercase, one number, and one special character"
       )
       .required("Enter Valid Password"),
@@ -40,65 +41,48 @@ export default function Login() {
       </div>
       <div className="pt-5 sm:pt-8 text-center">
         <p className="text-[24px] sm:text-[32px] md:text-[42px] font-semibold">
-          Login faster and safer to your <span className="text-bg-blue-12">Clarvion</span> account 😊
+          Login faster and safer to your <span className="text-bg-blue-12">Clarvion</span> account😊
         </p>
       </div>
       <div className="w-full sm:w-auto">
-        <div
-          className="container mx-auto px-5 sm:px-10 md:px-20 lg:px-40 xl:px-64 rounded-[10px] mt-[45px] py-[35px]"
-          style={{ backgroundColor: formBg }}
-        >
+        <div className={`container mx-auto px-5 sm:px-10 md:px-20 lg:px-40 xl:px-64 rounded-[10px] mt-[45px] py-[35px]`} style={{ backgroundColor: formBg }}>
           <Formik
             initialValues={{ email: "", password: "" }}
             validationSchema={validationSchema}
-            onSubmit={(values) => {
-              console.log(values);
-            }}
+            onSubmit={(values) => console.log(values)}
           >
-            {({ handleSubmit }) => (
+            {({ errors, touched, handleSubmit }) => (
               <form onSubmit={handleSubmit} className="grid grid-cols-1 gap-4">
                 <div className="flex flex-col gap-1 font-semibold">
                   <label className="text-[18px] sm:text-[24px] text-bg-blue-11">Email</label>
-                  <Field
-                    type="email"
-                    name="email"
-                    data-testid="email"
-                    className="w-full sm:w-[433px] h-[50px] bg-white sm:h-[60px] border-[1px] border-black rounded-[4px] px-4 text-[18px] focus:outline-none focus:border-gray-200 focus:shadow-lg focus:shadow-bg-blue-14"
-                  />
+                  <Field type="email" name="email" data-testid="email" className={`w-full sm:w-[433px] h-[50px] sm:h-[60px] border-[1px] border-black rounded-[4px] px-4 text-[18px] bg-white focus:outline-none focus:border-gray-200 focus:shadow-lg focus:shadow-bg-blue-14 transition-transform duration-300 ease-in-out ${errors.email && touched.email ? 'border-red-500' : ''}`} />
                   <ErrorMessage name="email" component="div" className="text-red-500 text-[16px]" />
                 </div>
                 <div className="flex flex-col gap-1 font-semibold">
                   <label className="text-[18px] sm:text-[24px] text-bg-blue-11">Password</label>
-                  <Field
-                    type="password"
-                    name="password"
-                    data-testid="pass"
-                    className="w-full sm:w-[433px] h-[50px] bg-white sm:h-[60px] border-[1px] border-black rounded-[4px] px-4 text-[18px] focus:outline-none focus:border-gray-200 focus:shadow-lg focus:shadow-bg-blue-14"
-                  />
+                  <Field type="password" name="password" data-testid="pass" className={`w-full sm:w-[433px] h-[50px] sm:h-[60px] border-[1px] border-black rounded-[4px] px-4 text-[18px] bg-white focus:outline-none focus:border-gray-200 focus:shadow-lg focus:shadow-bg-blue-14 transition-transform duration-300 ease-in-out ${errors.password && touched.password ? 'border-red-500' : ''}`} />
                   <ErrorMessage name="password" component="div" className="text-red-500 text-[16px] text-wrap w-full sm:w-[433px]" />
                 </div>
-                <div className="mt-4 md:w-[433px] w-full">
-                  <button type="submit" data-testid="nextButton" className="btn bg-bg-blue-12 hover:bg-bg-blue-11 text-white w-full p-3 rounded-md">
-                    Next
-                  </button>
-                </div>
+                <button type="submit" data-testid="nextButton" onClick={handleNextClick} className="btn bg-bg-blue-12 hover:bg-bg-blue-11 text-white w-full p-3 rounded-md">
+                  <span className="text-[18px] md:text-[24px]">Next</span>
+                </button>
               </form>
             )}
           </Formik>
           <div className="flex flex-col gap-5 mt-6">
-            <Link href="#" className="flex items-center gap-[45px] bg-white border-[0.8px] rounded-md border-black p-3 hover:shadow-lg">
+            <Link href="#" className="flex items-center gap-[45px] bg-white border-[0.8px] rounded-md border-black p-3 hover:shadow-lg hover:shadow-bg-blue-14">
               <img src="google.svg" className="w-6 h-6" alt="Google" />
               <span className="text-[16px] sm:text-[20px]">Login with Google</span>
             </Link>
-            <Link href="#" className="flex items-center gap-[39px] bg-white border-[0.8px] rounded-md border-black p-3 hover:shadow-lg">
+            <Link href="#" className="flex items-center gap-[39px] bg-white border-[0.8px] rounded-md border-black p-3 hover:shadow-lg hover:shadow-bg-blue-14">
               <img src="linkedin.svg" className="w-8 h-8" alt="LinkedIn" />
               <span className="text-[16px] sm:text-[20px]">Login with LinkedIn</span>
             </Link>
           </div>
-          <footer className="mt-12 text-[14px] sm:text-[20px] text-center">
-            ©2024 Clarvion, Inc. All Rights Reserved. <span className="text-bg-blue-12 cursor-pointer font-semibold">Privacy Policy & Manage Cookies</span>
-          </footer>
         </div>
+        <footer className="mt-12 text-[14px] sm:text-[20px] text-center">
+          ©2024 Clarvion, Inc. All Rights Reserved. <span className="text-bg-blue-12 cursor-pointer font-semibold">Privacy Policy & Manage Cookies</span>
+        </footer>
       </div>
     </div>
   );
