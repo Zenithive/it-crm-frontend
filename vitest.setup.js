71ef6bc9733abec31062a14e30b437a9e63bb7ca
Arati Dangar <aratid@zenithive.com>
2025-02-03 15:10:15 +0530

    vitest file deleted

--- vitest.setup.js
+++
@@ -1,9 +0,0 @@
-
-import { expect } from 'vitest';
-import '@testing-library/jest-dom/vitest';
-global.ResizeObserver = class {
-    observe() {}
-    unobserve() {}
-    disconnect() {}
-  };
-  global.expect = expect;
