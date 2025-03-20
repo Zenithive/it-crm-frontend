cb4d6ebcc360f59669ec09e856bdebca3d934894
srt-1909 <sachinthakkar1992002@gmail.com>
2025-03-20 11:17:20 +0530

    dashboard minor changes

--- app/components/Dashboard/DashboardTitle.tsx
+++ app/components/Dashboard/DashboardTitle.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useRouter } from "next/navigation";
 import Title from "../../microComponents/Title";
 import HeaderFlag from "../../microComponents/HeaderFlag";
 
@@ -7,15 +8,21 @@ interface DashboardTitleProps {
 }
 
 const Dashboard_Title: React.FC<DashboardTitleProps> = ({ Dashboardtitle }) => {
+  const router = useRouter();
+
+  const handleLeadClick = () => {
+    router.push('/contact') 
+  };
+
   return (
     <div>
       <div className="flex justify-between items-center">
         <Title title={Dashboardtitle[0].titleName} />
 
         <div className="flex items-center space-x-4">
-          <HeaderFlag/>
-          <button className="lead_button">
-            <img src="lead_icon.svg" alt="icon" className="p-2"></img>
+          <HeaderFlag />
+          <button className="lead_button" onClick={handleLeadClick} type="button">
+            <img src="lead_icon.svg" alt="icon" className="p-2" />
             Lead
           </button>
         </div>
